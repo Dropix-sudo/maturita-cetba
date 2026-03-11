521e8eafe18decf607ebfd37de1357f6ca4a582c
Dropix <r5charda@gmail.com>
2026-03-11 20:24:57 +0100

    Remove unused fetch event listener
    Remove unused fetch event listener from service worker.

--- sw.js
+++ sw.js
@@ -1,11 +1,9 @@
 self.addEventListener('install', (e) => {
     self.skipWaiting();
 });
-
 self.addEventListener('activate', (e) => {
     e.waitUntil(clients.claim());
 });
-
 self.addEventListener('fetch', (e) => {
-    // Prohlížeč vyžaduje fetch event, aby uznal aplikaci jako instalovatelnou
+    // Toto je zde naschvál prázdné, aby Chrome povolil instalaci
 });
